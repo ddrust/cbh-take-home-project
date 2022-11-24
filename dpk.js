7acc63204bad7f2a828cf1c9dcfc2c7cc3abab1d
ddrust <davidjramirez0611@gmail.com>
2022-11-24 18:47:29 -0300

    add comment

--- dpk.js
+++ dpk.js
@@ -2,7 +2,7 @@ const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
+  //const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate = TRIVIAL_PARTITION_KEY;
 
   if(event){
@@ -14,6 +14,9 @@ exports.deterministicPartitionKey = (event) => {
       candidate = crypto.createHash("sha3-512").update(data).digest("hex");
     }
   }
-  
+
   return candidate;
 };
+
+//The logic of the mechanism which is originally simple has become complicated than necessity.
+//Considering the flow of the code, I refactored by merging cases and removing extra falling points.
